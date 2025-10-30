36ee40ce5aef7f7848de49fb0ae2ffc945bc3ae8
saeed <saem1721364@gmail.com>
2025-10-30 11:41:52 +0330

    Add the route to the functions to edit and delete a product in the productRoutes file in the backend folder.

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,12 +1,18 @@
 // src/routes/productRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, addProduct } = require('../controllers/productController');
+const { getAllProducts, addProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 
 // مسیر اصلی برای محصولات
 router.get('/', getAllProducts);
 
 // اضافه کردن محصول جدید
 router.post('/', addProduct);
+
+// ویرایش محصول
+router.put('/:id', updateProduct);
+
+// حذف محصول
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
